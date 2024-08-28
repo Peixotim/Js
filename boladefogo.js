207ccd693413187e7c10ba01c13cf26e7c65b53e
Pedro Peixoto <126399622+Peixotim@users.noreply.github.com>
2024-08-28 10:13:42 -0300

    Add files via upload

--- boladefogo.js
+++ boladefogo.js
@@ -1,7 +1,40 @@
-    function interact(e){
-        var npc = e.npc
-        BolaDeFogo(npc,3,3,6,"sou_fire_flame")
-        BolaDeFogo(npc,0.5,1,6,"end_rod")
+function dialogClose(e){
+    var player = e.player
+    var npc = e.npc
+    
+    var angle = 0 //Este angulo e baseado em um triangulo ou sejá 45 graus, 90 graus e por ae vai, como a bola de fogo e continua e reta será 0
+    var pitch = 40 //Altura onde a bola de fogo irá spawnar
+    var distance = 1 //Essa distancia tem que ser continua ou sejá começar do 1 e ir até o 10
+
+    for(var i = 0;i<36;++i){ 
+        for(var j = -9;j<9;++j){ 
+        var deltas = FrontVectors(npc,i*10,j*10,pitch,0)
+
+
+        var X = npc.x +deltas[0]
+        var Y = npc.y+1+deltas[1]
+        var Z = npc.z +deltas[2]
+        npc.world.spawnParticle("flame",X,Y,Z,0,0,0,0,1)
+        }
     }
 
-//BolaDeFogo(npc,3,3,6,"sou_fire_flame") 2 variação
+
+}
+function FrontVectors(entity,dr,dp,distance,Mode){
+    if(Mode == 0){
+        var Angle = dr*Math.PI/180
+        var Pitch = dp*Math.PI/180
+    }
+    if(Mode == 1){
+        var Angle = (dr + entity.getRotation())*Math.PI/180
+        var Pitch = (dp + -entity.getPitch())*Math.PI/180
+    }
+
+ 
+    var dx = -Math.sin(Angle)*distance*Math.cos(Pitch)
+    var dy = Math.sin(Pitch)*distance
+    var dz = Math.cos(Angle)*distance*Math.cos(Pitch)
+    return[dx,dy,dz];//O array é este
+}
+
+// talvez adicionar negativo no var dx = -Math.sin(angle)
---
+++ transf.js
@@ -0,0 +1,17 @@
+function interact(e){
+  // var player = e.source
+  var player = e.player
+  var item = e.player.getInventoryHeldItem()
+
+  var conversa = "lSeu Tp foi setado bom aproveito!"
+  var conversa2 = "§4§lVocê não possui o papel requisitado!"
+
+  if(item != null && item.getDisplayName() == "§4§l- 10 Milhões de TP -"){
+    var nome = player.getName()
+    e.npc.executeCommand("jrmctp 10000000 " + nome);
+    e.player.removeItem(item, 1)
+   e.npc.say(conversa)
+  } else{
+ e.npc.say(conversa2)
+  }
+}
